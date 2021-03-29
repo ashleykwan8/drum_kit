2fff4ece86ec3735cd013994c46cfd9b66fab922
Ashley Kwan <ashleykwan888@gmail.com>
2021-03-29 15:26:08 -0700

    added for loop for all the buttons to listen for the eventlistener

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 
-// this targets the first button and "listens" for a click event to occur to call the function
-document.querySelector("button").addEventListener('click', handleClick)
+// length of the number of buttons on the page
+const numOfDrumButtons = document.querySelectorAll(".drum").length;
+
+// use For Loop to go through all the buttons to handle the event listener 
+for (var i=0; i<numOfDrumButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener('click', handleClick);
+}
+
+
+// targets the first button and "listens" for a click event to occur to call the function
+// document.querySelector("button").addEventListener('click', handleClick);
 
 function handleClick () {
     alert("I got clicked!");
