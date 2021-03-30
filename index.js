fd2ffde7d03d7c150ca1b7e482f9b49bccac3015
Ashley Kwan <ashleykwan888@gmail.com>
2021-03-30 09:29:34 -0700

    added sounds to event listener

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const numOfDrumButtons = document.querySelectorAll(".drum").length;
 
 // use For Loop to go through all the buttons to handle the event listener 
 for (var i=0; i<numOfDrumButtons; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener('click', handleClick);
+    document.querySelectorAll(".drum")[i].addEventListener('click', playSound);
 }
 
 
 // targets the first button and "listens" for a click event to occur to call the function
 // document.querySelector("button").addEventListener('click', handleClick);
 
-function handleClick () {
-    alert("I got clicked!");
+function playSound () {
+    var audio = new Audio('sounds/tom-1.mp3');
+    audio.play();
 }
